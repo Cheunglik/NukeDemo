7af50959113892a2cfc589e36dd6727e8837d240
Lik <lik1018@gmail.com>
2025-12-21 17:05:42 +0800

    first upload ignore

---
+++ NukeDemo/StringExtension.cs
@@ -0,0 +1,11 @@
+﻿namespace NukeDemo
+{
+    public static class StringExtension
+    {
+        static string chinesesRange = @"[\u4e00-\u9fff]";
+        public static bool HasChinese(this string str)
+        {
+            return System.Text.RegularExpressions.Regex.IsMatch(str, chinesesRange);
+        }
+    }
+}
---
+++ NukeDemoTests/StringExtensionTests.cs
@@ -0,0 +1,23 @@
+﻿using Xunit;
+using NukeDemo;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NukeDemo.Tests
+{
+    public class StringExtensionTests
+    {
+        [Fact()]
+        public void HasChineseTest()
+        {
+            var result = true;
+            var test = "測試";
+
+            //Xunit.Assert.Fail("This test needs an implementation");
+            Assert.Equal(result, test.HasChinese());
+        }
+    }
+}
