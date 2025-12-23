4270c843aa6ddf98526f2aa1b7cad7e7a1ab2457
alex <alex.cheung.job@gmail.com>
2025-12-23 12:16:57 +0800

    add nuget from nuke push

---
+++ NugetDemo/Program.cs
@@ -0,0 +1,15 @@
+﻿using NukeDemo;
+
+namespace NugetDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            var test = "1/2";
+            var value = test.ParseToDouble();
+            Console.WriteLine($"{test} value :{value}");
+        }
+    }
+}
